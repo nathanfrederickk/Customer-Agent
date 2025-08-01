33bc84a6e51436fd38ffc5308d5200662b459151
Frederick <fthu0001@student.monash.edu>
2025-08-01 21:17:28 +1000

    replaced account id and region with env

--- cdk/bin/cdk.js
+++ cdk/bin/cdk.js
@@ -2,6 +2,7 @@
 
 const cdk = require('aws-cdk-lib');
 const {CustomerAgentStack } = require('../lib/cdk-stack');
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 
 const app = new cdk.App();
 new CustomerAgentStack(app, 'CustomerAgentStack', {
@@ -9,13 +10,9 @@ new CustomerAgentStack(app, 'CustomerAgentStack', {
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  env: { account: '713881796527', region: 'ap-southeast-2' },
+  env: { account: process.env.AWS_ACCOUNT_ID, region: process.env.AWS_REGION  },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
